4ad4d292176ad3945355fda188d990e297036a18
Norana <noranalisha@gmail.com>
2020-09-03 08:36:25 +0100

    Prepare and Sending Signup request

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -31,8 +31,11 @@ export class DataStorageService {
     )
     .pipe(map(recipes => {
       return recipes.map(recipe=> {
-        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
-      })
+        return {
+          ...recipe,
+          ingredients: recipe.ingredients ? recipe.ingredients : []
+        };
+      });
     }),
     tap(recipes => {
       this.recipeService.setRecipes(recipes);
