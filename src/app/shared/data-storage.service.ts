11cbbc9b934c897c07d0a7dee1859ddcb92cc949
Norana <noranalisha@gmail.com>
2020-09-05 07:37:35 +0100

    Add the token to Out going request

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,56 +1,59 @@
+import { AuthService } from "./../auth/auth.service";
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { map, tap } from "rxjs/operators";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { map, tap, take, exhaustMap } from "rxjs/operators";
 
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
 
 @Injectable({
-  providedIn: 'root'
-})//we inject recipeService n http service
+  providedIn: "root",
+}) //we inject recipeService n http service
 export class DataStorageService {
-
-  constructor(private http: HttpClient,
-    private recipeService: RecipeService){}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   //storeRecipes(recipes: Recipe[]){}
-  storeRecipes(){
+  storeRecipes() {
     const recipes = this.recipeService.getRecipes();
-    this.http.put(
-      'https://ngcourse-recipebook-ef7ea.firebaseio.com/recipes.json',
-      recipes
-    )
-    .subscribe( response => {
-      console.log( response);
-    });
-  }
-
-  fetchrecipes(){
-    return this.http.get<Recipe[]>(
-      'https://ngcourse-recipebook-ef7ea.firebaseio.com/recipes.json'
-    )
-    .pipe(map(recipes => {
-      return recipes.map(recipe=> {
-        return {
-          ...recipe,
-          ingredients: recipe.ingredients ? recipe.ingredients : []
-        };
+    this.http
+      .put(
+        "https://ngcourse-recipebook-ef7ea.firebaseio.com/recipes.json",
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
       });
-    }),
-    tap(recipes => {
-      this.recipeService.setRecipes(recipes);
-    })
-    )
-   /* .subscribe(recipes => {
-      //console.log(recipes);
-      this.recipeService.setRecipes(recipes);
-    })*/
-
   }
 
-
+  fetchrecipes() {
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        return this.http.get<Recipe[]>(
+          "https://ngcourse-recipebook-ef7ea.firebaseio.com/recipes.json",
+          {
+            params: new HttpParams().set('auth', user.token)
+          }
+        );
+      }),
+      map((recipes) => {
+        return recipes.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+      tap((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      })
+    );
+  }
 }
-
 
 //need to import Httpclientmodule in app module
 //PUT is writing data in Firebase same like post or over writing
