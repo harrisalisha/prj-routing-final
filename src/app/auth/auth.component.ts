7527c81d09d4f1e8a7c603403957dda57c905571
Norana <noranalisha@gmail.com>
2020-09-07 17:11:10 +0100

    Data Binding and Event Binding

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
-import { Component, ComponentFactoryResolver, ViewChild } from "@angular/core";
+import { Component, ComponentFactoryResolver, ViewChild, OnDestroy } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthService ,  AuthResponseData } from "./auth.service";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 import { Router } from "@angular/router";
 import { AlertComponent} from '../shared/alert/alert.component';
 import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
@@ -10,13 +10,16 @@ import { PlaceholderDirective } from "../shared/placeholder/placeholder.directiv
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
   isLoginMode = true;
   error: string = null;
   isLoading = false;
   @ViewChild(PlaceholderDirective, {static: false}) alertHost : PlaceholderDirective;// we pass type
 
-  constructor(private authService: AuthService,
+  private closeSub: Subscription;
+
+  constructor(
+    private authService: AuthService,
     private router: Router,
     private componentFactoryResolver: ComponentFactoryResolver){}
 
@@ -60,6 +63,12 @@ export class AuthComponent {
     this.error = null;
   }
 
+  ngOnDestroy(){
+    if(this.closeSub){
+      this.closeSub.unsubscribe();
+    }
+  }
+
   private showErrorAlert(message: string){
     //const alertCmp = new AlertComponent();//valid code not valid in angular
     //obj that knows how to create component
@@ -68,8 +77,12 @@ export class AuthComponent {
     const hostViewContainerRef = this.alertHost.viewContainerRef;//obj that allow you to interact w the DOm
     hostViewContainerRef.clear();
 
-    hostViewContainerRef.createComponent(alertCmpFactory);
-
+    const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
+    componentRef.instance.message = message;
+    this.closeSub = componentRef.instance.close.subscribe(()=> {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });//ok,rule its exception
   }
 
 }
